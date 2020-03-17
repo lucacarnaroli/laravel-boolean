dcdd8dd1bf4629d9746ebb4d0fde1b1321881bf5
luca <luca.carnaroli98@gmail.com>
2020-03-17 16:13:25 +0100

    creazione controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel</title> -->
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
 
         <!-- Styles -->
-        <style>
+        <!-- <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
@@ -97,4 +97,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> -->
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// homepage
+Route::get('/','HomeController@index')->name('home.home');
+
+Route::get('/privacy','HomeController@privacy')->name('home.privacy');
+
+Route::get('/domande-frequenti','HomeController@faq')->name('home.faq');
+
+// studenti
+Route::get('/students','StudentController@index')->name('student.index');
+Route::get('/students/show/{id}','StudentController@show')->name('student.show');
